f2eaa7e82bd429c875d07b6644ccc652abb70fa5
adahake <56040189+adahake@users.noreply.github.com>
2020-08-16 23:34:20 +0530

    Add files via upload

--- import-from-postman.bat
+++ import-from-postman.bat
@@ -11,6 +11,6 @@ SET postman_collection_id="10899512-4e24b7bb-9223-4a77-87a6-a95fd76ade4f"
 SET postman_collection_env="10899512-110ebedf-de77-48c7-b2c7-a2bb85a70cd4"
 if not exist "%target_folder%" mkdir %target_folder%
 
-curl -X GET https://api.getpostman.com/collections/%postman_collection_id% -H "X-Api-Key: %postman_api_key%" -H "Cache-Control: no-cache" -o %target_folder%\collection.json
-curl -X GET https://api.getpostman.com/environments/%postman_collection_env% -H "X-Api-Key: %postman_api_key%" -H "Cache-Control: no-cache" -o %target_folder%\environment.json
+curl -X GET https://api.getpostman.com/collections/%postman_collection_id% -H "X-Api-Key: %postman_api_key%" -H "Cache-Control: no-cache" -o %target_folder%\Insurance.postman_collection.json.json
+curl -X GET https://api.getpostman.com/environments/%postman_collection_env% -H "X-Api-Key: %postman_api_key%" -H "Cache-Control: no-cache" -o %target_folder%\Insurance-Test2.postman_environment.json
 
